a6ed34526a234bd0d8fff6ba2927da49b8865776
thuyanduong-flatiron <thuyanannduong@gmail.com>
2019-04-25 14:42:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+class Dog {
+  constructor(name, sex){
+    this.sex = sex
+    this.name = name
+  }
+
+  speak(){
+    return `${this.name} says woof!`
+  }
+}
+
+class Cat {
+  constructor(name, sex){
+    this.sex = sex
+    this.name = name
+  }
+
+  speak(){
+    return `${this.name} says meow!`
+  }
+}
+
+class Bird {
+  constructor(name, sex){
+    this.sex = sex
+    this.name = name
+  }
+
+  speak(){
+    if(this.sex === 'female'){
+      return `${this.name} says squawk!`
+    }
+    return `It's me! ${this.name}, the parrot!`
+  }
+}
